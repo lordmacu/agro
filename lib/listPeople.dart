19642458eb6802e3e02083c63b70bb96b1ac0685
Cristian garcia <cristian.garcia@pegasi.io>
2021-05-18 19:21:23 -0500

    dinamic employess

--- lib/listPeople.dart
+++ lib/listPeople.dart
@@ -1,5 +1,13 @@
 
 import 'package:agrotest/helpers.dart';
+import 'package:agrotest/daos/EmployeeDao.dart';
+import 'package:agrotest/history.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqfly/sqfly.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:agrotest/models/Employs.dart';
+
 import 'package:flutter/material.dart';
 class ListPeople extends StatefulWidget {
   ListPeople({Key key,this.listPeopleArray}) : super(key: key);
@@ -34,6 +42,82 @@ class _ListPeople extends State<ListPeople> {
 
     print("aqui esta la lista de cosas   ${widget.listPeopleArray}");
   }
+  Future loadData() async {
+    String url = "https://calidad.smartcompanies.com.co";
+
+    final sqfly = await Sqfly.initialize(
+      /// database named
+      name: 'datdacdddddddddddsdddsdd',
+      // database version
+      version: 2,
+      logger: false,
+
+      daos: [
+        EmployeeDao(),
+
+      ],
+    );
+
+    _onLoading();
+
+    var client = http.Client();
+
+    var getEmployee = await client.get(Uri.parse('${url}/getEmployee'));
+    var responsegetEmployee = jsonDecode(getEmployee.body);
+
+    List<Employs> employs = [];
+    for (var i = 0; i < responsegetEmployee.length; i++) {
+      Employs dropdown = Employs();
+      dropdown.name = responsegetEmployee[i]["name"];
+      dropdown.sede = responsegetEmployee[i]["sede_id"];
+      dropdown.id = responsegetEmployee[i]["id"];
+      dropdown.type = responsegetEmployee[i]["type_position"];
+      dropdown.position = responsegetEmployee[i]["position"];
+      employs.add(dropdown);
+    }
+
+    sqfly<EmployeeDao>().destroyAll();// insertAll
+
+   await sqfly<EmployeeDao>().createAll(employs); // insertAll
+
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    int sede=prefs.getInt("sede");
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HistoryPage(sede: sede)),
+    );
+  }
+
+  void _onLoading() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white.withOpacity(0.1),
+          elevation: 0,
+          child: Container(
+            padding: EdgeInsets.all(30),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                new CircularProgressIndicator(),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Cargando",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,20 +126,35 @@ class _ListPeople extends State<ListPeople> {
 
       appBar: AppBar(
         title: Container(
-          child: TextFormField(
-            onChanged: (value){
-              Helpers helpers = Helpers();
+          child: Row(
+            children: [
+              Expanded(child: TextFormField(
+                onChanged: (value){
+                  Helpers helpers = Helpers();
 
-              List<String> people= helpers.filterStrings(widget.listPeopleArray, value);
-              setState(() {
-                if(people.length>0){
-                  savedListPeopleArray=people;
-                }else{
-                  savedListPeopleArray=widget.listPeopleArray;
-                }
-              });
+                  List<String> people= helpers.filterStrings(widget.listPeopleArray, value);
+                  setState(() {
+                    if(people.length>0){
+                      savedListPeopleArray=people;
+                    }else{
+                      savedListPeopleArray=widget.listPeopleArray;
+                    }
+                  });
 
-            },
+                },
+              ),
+              flex: 7,),
+              Expanded(child: InkWell(
+                child: Container(
+                  padding: EdgeInsets.only(left: 10,top: 10,bottom: 10,right: 10),
+                  child: Icon(Icons.refresh),
+                ),
+                onTap: (){
+                  loadData();
+
+                },
+              ),flex: 1,)
+            ],
           ),
         ),
       ),
